52b70615ebb95cb2b4c54ad95d020d5b578ae720
Akshay <akshaysv957@gmail.com>
2023-01-10 11:58:10 +0530

    second commit

--- src/components/RowPost/RowPost.jsx
+++ src/components/RowPost/RowPost.jsx
@@ -16,33 +16,34 @@ const RowPost=(props) => {
     })
   },[])
 
-  const opts = {
-      height: '390',
-      width: '100%',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 0,
+const opts = {
+  height: "390",
+  width: "100%",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  },
+};
+const movieTrailer = (id) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`
+    )
+    .then((response) => {
+      if (response.data.results.length !== 0) {
+        setUrlId(response.data.results[0]);
+      } else {
+        console.log("Array is empty");
       }
-    };
-
-    const handleMovie = (id) =>{
-      console.log(id)
-      axios.get(`/movie/${id}/videos?api_keys=${API_KEY}&language=en-US`).then((response)=>{
-        console.log(response.data.results)
-        if(response.data.results.length!==0){
-          setUrlId(response.data.results[0])
-        }else{
-          console.log('Empty Array')
-        }
-      })
-    }
+    });
+};
 
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
         {movies.map((obj) => (
-          <img onClick={()=>handleMovie(obj.id)}
+          <img onClick={()=>movieTrailer(obj.id)}
             className={props.isSmall ? "small_poster" : "poster"}
             src={`${imageUrl + obj.backdrop_path}`}
             alt="poster"
